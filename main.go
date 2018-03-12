89b206a9688f8b2ffff93c20608da75e765accf1
InitialShape <InitialShape@mailbox.org>
2018-03-12 15:56:28 +0100

    Run go fmt

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func contentHandler(w http.ResponseWriter, r *http.Request) {
 	files := readFiles()
 	for _, file := range files {
 		if "content/"+file.Name() == r.URL.Path[1:] {
-		        filePath := "./content/" + file.Name()
+			filePath := "./content/" + file.Name()
 			b, err := ioutil.ReadFile(filePath)
 			if err != nil {
 				log.Fatal(err)
@@ -28,9 +28,9 @@ func contentHandler(w http.ResponseWriter, r *http.Request) {
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	files := readFiles()
 	t, err := template.ParseFiles("./templates/index.html")
-        if err != nil {
-            log.Fatal(err)
-        }
+	if err != nil {
+		log.Fatal(err)
+	}
 	t.Execute(w, &files)
 
 }
@@ -47,8 +47,8 @@ func readFiles() []os.FileInfo {
 func main() {
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/content/", contentHandler)
-        fs := http.FileServer(http.Dir("static"))
-        http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
