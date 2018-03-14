4b53fdd764cb15364702eab2fa584bcd4661f59b
InitialShape <InitialShape@mailbox.org>
2018-03-14 14:50:56 +0100

    Add sorting of Articles

--- main.go
+++ main.go
@@ -7,17 +7,35 @@ import (
 	"log"
 	"net/http"
 	"os"
+        "sort"
 	"path/filepath"
 	"regexp"
+        "time"
+        "strings"
 )
 
 type Article struct {
 	Path string
 	Info os.FileInfo
+        CreatedAt time.Time
+}
+
+type Articles []Article
+
+func (p Articles) Len() int {
+    return len(p)
+}
+
+func (p Articles) Less(i, j int) bool {
+    return p[i].CreatedAt.After(p[j].CreatedAt)
+}
+
+func (p Articles) Swap(i, j int) {
+    p[i], p[j] = p[j], p[i]
 }
 
 func contentHandler(w http.ResponseWriter, r *http.Request) {
-	files, err := readDirectory()
+	files, err := getArticles()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,31 +54,58 @@ func contentHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	files, err := readDirectory()
+	articles, err := getArticles()
+        sort.Sort(articles)
 	t, err := template.ParseFiles("./templates/index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	t.Execute(w, &files)
+	t.Execute(w, &articles)
 
 }
 
-func readDirectory() ([]Article, error) {
-	var articles []Article
+func pathToTime(path string) (time.Time, error) {
+        first := strings.IndexByte(path, '/')+1
+        last := strings.LastIndex(path, "/")
+        dateString := path[first:last]
+        date, err := time.Parse("2006/03/02", dateString)
+
+        if err != nil {
+                log.Fatal(err)
+                return time.Time{}, err
+        }
+
+        return date, err
+}
+
+
+func getArticles() (Articles, error) {
+	var articles Articles
 	var err error
 
 	err = filepath.Walk("./content", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Print(err)
-			return nil
-		}
+                if err != nil {
+                        log.Fatal(err)
+                        return nil
+                }
 		match, err := regexp.MatchString(".md", info.Name())
 		if match {
-			article := Article{path, info}
+
+                        date, err := pathToTime(path)
+                        if err != nil {
+                                log.Fatal(err)
+                                return nil
+                        }
+
+                        article := Article{path, info, date}
 			articles = append(articles, article)
 		}
 		return nil
 	})
+
+        if err != nil {
+            return nil, err
+        }
 
 	return articles, err
 }
